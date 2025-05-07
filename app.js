9f04cfebd1e1b582f10b187c317775477e64f728
PrateekP <prateekpanta1789@gmail.com>
2025-05-07 14:04:11 +0530

    Added functionality to add task with checklist

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ class ToDoApp {
     constructor() {
         this.taskList = document.querySelector(".task-list");
         this.taskInput = document.querySelector("#task-input");
-        this.addBtn = document.querySelector(".add-task-btn");
         this.totalTaskCounter = document.querySelector(".total-task");
         this.completedTaskCounter = document.querySelector(".task-completed");
+        this.checkListArray = [];
         this.task = '';
         this.totalTasks = 0;
         this.completedTask = 0;
@@ -24,7 +24,7 @@ class ToDoApp {
     checkTask(event) {
         if (event.target.closest(".add-task-btn") && this.taskInput.value) {
             this.task = this.taskInput.value;
-            this.generateTaskItem(this.task);
+            this.generateTaskItem(this.task, true);
             this.taskInput.value = '';
         }
         if (event.target.closest(".delete-task")) {
@@ -34,33 +34,81 @@ class ToDoApp {
             this.totalTaskCounter.innerHTML = this.totalTasks;
         }
     }
-    
-    generateTaskItem(task) {
-        const newTaskItem = document.createElement("li");
-        newTaskItem.className = "task-list-item";
-        newTaskItem.innerHTML = `<div class="task-head">
-                                    <div class="">
-                                        <input type="checkbox" name="task_complete" id="task_check${this.itemNum}" style="display: none;">
-                                        <label for="task_check${this.itemNum}"><img src="./assets/check.svg" alt=""></label>
-                                        <h4 class="task">${task}</h4>
-                                    </div>
-                                    <div class="task-actions">
-                                        <button class="edit-task">
-                                            <img src="./assets/edit.svg" alt="Edit icon">
-                                        </button>
-                                        <button class="insert-checklist">
-                                            <img src="./assets/add-checklist.svg" alt="Add check-list icon">
-                                        </button>
-                                        <button class="delete-task">
-                                            <img src="./assets/delete.svg" alt="Delete Icon">
-                                        </button>
-                                    </div>
-                                </div>`;
 
-        this.taskList.appendChild(newTaskItem);
+    generateTaskItem(task, isChecklist = false) {
+        if (isChecklist) {
+            let taskString = task.split(".");
+            let newTask = taskString.shift();
+
+            const newTaskItem = document.createElement("li");
+            newTaskItem.className = "task-list-item";
+            newTaskItem.innerHTML = `<div class="task-head">
+                                        <div class="">
+                                            <input type="checkbox" name="task_complete" id="task_check${this.itemNum}" style="display: none;">
+                                            <label for="task_check${this.itemNum}"><img src="./assets/check.svg" alt=""></label>
+                                            <h4 class="task">${newTask}</h4>
+                                        </div>
+                                        <div class="task-actions">
+                                            <button class="edit-task">
+                                                <img src="./assets/edit.svg" alt="Edit icon">
+                                            </button>
+                                            <button class="insert-checklist">
+                                                <img src="./assets/add-checklist.svg" alt="Add check-list icon">
+                                            </button>
+                                            <button class="delete-task">
+                                                <img src="./assets/delete.svg" alt="Delete Icon">
+                                            </button>
+                                        </div>
+                                    </div>
+                                    <ul class="check-list"></ul>`;
+
+            this.taskList.appendChild(newTaskItem);
+
+            this.checkListArray = [...(document.querySelectorAll(".check-list"))];
+
+            taskString.forEach((itm) => {
+                const child = document.createElement("li");
+                child.className = "check-list-item";
+                child.innerHTML = `<input type="checkbox" name="user_task_checklist" id="">
+                                   <label for="">${itm}</label>`;
+                this.checkListArray[this.itemNum - 1].appendChild(child);
+            });
+
+            console.log(this.itemNum - 1);
+            console.log(this.checkListArray[this.itemNum - 1]);
+            console.log(this.checkListArray);
+            taskString = '';
+            newTask = '';
+        }
+        else {
+            const newTaskItem = document.createElement("li");
+            newTaskItem.className = "task-list-item";
+            newTaskItem.innerHTML = `<div class="task-head">
+                                        <div class="">
+                                            <input type="checkbox" name="task_complete" id="task_check${this.itemNum}" style="display: none;">
+                                            <label for="task_check${this.itemNum}"><img src="./assets/check.svg" alt=""></label>
+                                            <h4 class="task">${task}</h4>
+                                        </div>
+                                        <div class="task-actions">
+                                            <button class="edit-task">
+                                                <img src="./assets/edit.svg" alt="Edit icon">
+                                            </button>
+                                            <button class="insert-checklist">
+                                                <img src="./assets/add-checklist.svg" alt="Add check-list icon">
+                                            </button>
+                                            <button class="delete-task">
+                                                <img src="./assets/delete.svg" alt="Delete Icon">
+                                            </button>
+                                        </div>
+                                    </div>`;
+            this.taskList.appendChild(newTaskItem);
+        }
+        
         this.totalTasks = [...document.querySelectorAll(".task-list-item")].length;
         this.totalTaskCounter.innerHTML = this.totalTasks;
     }
 }
 
 const myApp = new ToDoApp();
+
+/* Mock input: New Task 1.add more 1.add more 2.add more 3 */
