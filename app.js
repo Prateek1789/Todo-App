4f6f3f0be194d714d2cd6fdc3fdf833f0a75f36b
PrateekP <prateekpanta1789@gmail.com>
2025-05-12 15:39:10 +0530

    Refactored code for separation of concerns reenforced DRY principle

--- app.js
+++ app.js
@@ -1,119 +1,110 @@
+class Task {
+    constructor(title, checklist = [], flag) {
+        this.title = title;
+        this.checklistItems = checklist;
+        this.isCompleted = false;
+        this.isChecklist = this.checklistItems.length > 0 && flag;
+    }
+
+    createTask(iD) {
+       return `<div class="task-head">
+                    <div class="">
+                        <input type="checkbox" name="task_complete" id="task_check${iD}" style="display: none;">
+                        <label for="task_check${iD}"><img src="./assets/check.svg" alt=""></label>
+                        <h4 class="task">${this.title}</h4>
+                    </div>
+                    <div class="task-actions">
+                        <button class="edit-task">
+                            <img src="./assets/edit.svg" alt="Edit icon">
+                        </button>
+                        <button class="delete-task">
+                            <img src="./assets/delete.svg" alt="Delete Icon">
+                        </button>
+                    </div>
+                </div>`;
+    }
+
+    renderTask(parent, iD) {
+        if (this.isChecklist) {
+            const newTaskItem = document.createElement("li");
+            newTaskItem.className = "task-list-item";
+            newTaskItem.innerHTML = this.createTask(iD);
+            newTaskItem.innerHTML += `\n<ul class="check-list"></ul>`;
+
+            parent.appendChild(newTaskItem);
+
+            const checkList = newTaskItem.querySelector(".check-list");
+
+            this.checklistItems.forEach((itm, idx) => {
+                const child = document.createElement("li");
+                child.className = "check-list-item";
+                child.innerHTML = `<input type="checkbox" name="user_task_checklist" id="check_list_task${idx + 1}">
+                                   <label for="check_list_task${idx + 1}">${itm}</label>`;
+                checkList.appendChild(child);
+            });
+        }
+        else {
+            const newTaskItem = document.createElement("li");
+            newTaskItem.className = "task-list-item";
+            newTaskItem.innerHTML = this.createTask(iD);
+            parent.appendChild(newTaskItem);
+        }
+    }
+}
+
 class ToDoApp {
     constructor() {
         this.taskList = document.querySelector(".task-list");
         this.taskInput = document.querySelector("#task-input");
         this.totalTaskCounter = document.querySelector(".total-task");
-        this.completedTaskCounter = document.querySelector(".task-completed");
+        this.compTaskCounter = document.querySelector(".task-completed");
         this.addCheckListBtn = document.querySelector(".add-checklist-btn");
-        this.task = '';
+        this.isCheckListActive = this.addCheckListBtn.classList.contains("active");
         this.totalTasks = 0;
         this.completedTask = 0;
-        this.checkBoxIDNum = 1;
+        this.taskID = 1;
         this.isEditModeOn = false;
-        this.isCheckListBtnOn = this.addCheckListBtn.classList.contains("active");
         this.init();
     }
 
     init() {
         document.addEventListener("click", e => {
-            this.checkTask(e);
+            this.appOperations(e);
         });
-
-        /* --This code is only here to keep the mock task item in total task count */
-        this.totalTasks = [...document.querySelectorAll(".task-list-item")].length;
-        this.totalTaskCounter.innerHTML = this.totalTasks;
     }
 
-    checkTask(event) {
-        if (event.target.closest(".add-checklist-btn")) {
+    countTask() {
+        this.totalTasks = [...document.querySelectorAll(".task-list-item")].length;
+        this.totalTaskCounter.textContent = this.totalTasks;
+    }
+
+    appOperations(e) {
+        if (e.target.closest(".add-checklist-btn")) {
             this.addCheckListBtn.classList.toggle("active");
-            this.isCheckListBtnOn = this.addCheckListBtn.classList.contains("active");
+            this.isCheckListActive = this.addCheckListBtn.classList.contains("active");
         }
 
-        if (event.target.closest(".add-task-btn") && this.taskInput.value && !this.isEditModeOn) {
-            this.task = this.taskInput.value;
-            this.generateTaskItem(this.task, this.isCheckListBtnOn);
+        if (e.target.closest(".add-task-btn") && this.taskInput.value && !this.isEditModeOn) {
+            const taskString = this.taskInput.value.split(".");
+            const taskName = taskString[0];
+            taskString.shift();
+            const newTask = new Task(taskName, taskString, this.isCheckListActive);
+            newTask.renderTask(this.taskList, this.taskID);
+            this.countTask();
             this.taskInput.value = '';
-            this.task = '';
+            this.taskID++;
         }
         
-        if (event.target.closest(".delete-task")) {
-            let parent = event.target.closest("li");
-            this.totalTasks--;
-            this.totalTaskCounter.innerHTML = this.totalTasks;
+        if (e.target.closest(".delete-task")) {
+            let parent = e.target.closest("li");
             requestAnimationFrame(() => {
                 parent.classList.add("delete");
             });
             parent.addEventListener("animationend", () => {
                 this.taskList.removeChild(parent);
+                this.countTask();
             }, { once: true});
         }
-    }
-
-    generateTaskItem(task, isChecklist = false) {
-        if (isChecklist) {
-            let taskString = task.split(".");
-            let newTask = taskString.shift();
-
-            const newTaskItem = document.createElement("li");
-            newTaskItem.className = "task-list-item";
-            newTaskItem.innerHTML = `<div class="task-head">
-                                        <div class="">
-                                            <input type="checkbox" name="task_complete" id="task_check${this.checkBoxIDNum}" style="display: none;">
-                                            <label for="task_check${this.checkBoxIDNum}"><img src="./assets/check.svg" alt=""></label>
-                                            <h4 class="task">${newTask}</h4>
-                                        </div>
-                                        <div class="task-actions">
-                                            <button class="edit-task">
-                                                <img src="./assets/edit.svg" alt="Edit icon">
-                                            </button>
-                                            <button class="delete-task">
-                                                <img src="./assets/delete.svg" alt="Delete Icon">
-                                            </button>
-                                        </div>
-                                    </div>
-                                    <ul class="check-list"></ul>`;
-
-            this.taskList.appendChild(newTaskItem);
-
-            const subList = newTaskItem.querySelector(".check-list");
-
-            taskString.forEach((itm, idx) => {
-                const child = document.createElement("li");
-                child.className = "check-list-item";
-                child.innerHTML = `<input type="checkbox" name="user_task_checklist" id="check_list_task${idx + 1}">
-                                   <label for="check_list_task${idx + 1}">${itm}</label>`;
-                subList.appendChild(child);
-            });
-
-            taskString = '';
-            newTask = '';
-            this.checkBoxIDNum++;
-        }
-        else {
-            const newTaskItem = document.createElement("li");
-            newTaskItem.className = "task-list-item";
-            newTaskItem.innerHTML = `<div class="task-head">
-                                        <div class="">
-                                            <input type="checkbox" name="task_complete" id="task_check${this.checkBoxIDNum}" style="display: none;">
-                                            <label for="task_check${this.checkBoxIDNum}"><img src="./assets/check.svg" alt=""></label>
-                                            <h4 class="task">${task}</h4>
-                                        </div>
-                                        <div class="task-actions">
-                                            <button class="edit-task">
-                                                <img src="./assets/edit.svg" alt="Edit icon">
-                                            </button>
-                                            <button class="delete-task">
-                                                <img src="./assets/delete.svg" alt="Delete Icon">
-                                            </button>
-                                        </div>
-                                    </div>`;
-            this.taskList.appendChild(newTaskItem);
-            this.checkBoxIDNum++;
-        }
-        
-        this.totalTasks = [...document.querySelectorAll(".task-list-item")].length;
-        this.totalTaskCounter.innerHTML = this.totalTasks;
     }
 }
 
