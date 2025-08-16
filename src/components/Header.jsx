f46d397a680dba3c5d0a1b4cdc49729ce0ddf86a
PrateekP <prateekpanta1789@gmail.com>
2025-08-16 22:20:30 +0530

    Added header contexts

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,17 @@
 import { useContext, useState } from "react"
+import { HeaderContext } from "../context/todoContext"
 
 const Header = () => {
-    /* const { numOfTodos, compTodos } = useContext(HeaderContext);
-    const progressBarWidth = () => (100 / numOfTodos) * compTodos; */
+    const { totalTodos, completedTodos } = useContext(HeaderContext);
+    const progressBarWidth = () => (100 / totalTodos) * completedTodos;
 
     return (
         <header className='w-full h-auto text-gray-800 p-4 rounded-lg flex flex-col gap-4'>
             <h1 className='text-xl'>Daily Tasks</h1>
-            <p className='text-md mb-2'>{/* compTodos ||  */0} / {/* numOfTodos */0} Tasks completed</p>
+            <p className='text-md mb-2'>{completedTodos || 0 } / {totalTodos} Tasks completed</p>
             <span className='progress-bar w-full h-4 rounded-sm bg-gray-200 flex p-[0.1rem]'>
                 <span className={`progress-bar-fill h-full bg-[hsl(0,0%,100%)] rounded-xs`}
-                      style={{ width: '20%' /* `${progressBarWidth()}%` */ }}></span>
+                      style={{ width: `${progressBarWidth()}%` }}></span>
             </span>
         </header>
     )
