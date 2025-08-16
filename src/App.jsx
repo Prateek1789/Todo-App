0c99478bbba0e81b0440187a0b9d5935baa3e647
PrateekP <prateekpanta1789@gmail.com>
2025-08-16 22:19:51 +0530

    Imported App context provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
 import './App.css'
+import { AppProvider } from './context/todoContext'
 
 import Header from './components/Header'
 import TaskArea from './components/TaskArea'
+import AddButton from './components/AddButton'
 import TodoForm from './components/TodoForm'
 
 function App() {
   return (
-    <>
+    <AppProvider>
       <Header />
       <TaskArea />
+      <AddButton />
       <TodoForm />
-    </>
+    </AppProvider>
   )
 }
 
