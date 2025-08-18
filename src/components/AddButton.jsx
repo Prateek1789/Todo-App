b58dbf6da0f3eea39a6b882b94950287700d2a53
PrateekP <prateekpanta1789@gmail.com>
2025-08-18 23:19:07 +0530

    Restyled add button

--- src/components/AddButton.jsx
+++ src/components/AddButton.jsx
@@ -5,12 +5,13 @@ const AddButton = () => {
     const { formState, setFormState } = useContext(FormContext);
 
     return (
-        <button className='add-btn w-16 h-16 rounded-full flex items-center justify-center absolute bottom-1 left-1/2 -translate-1/2'
+        <button className='add-btn w-82 h-12 rounded-lg text-gray-200 flex items-center justify-center absolute bottom-1 left-1/2 -translate-1/2'
                 aria-pressed={formState}
                 onClick={() => setFormState(!formState)}>
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="#ffffff" viewBox="0 0 24 24" >
+            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="#ffffff" viewBox="0 0 24 24" >
                 <path d="M3 13h8v8h2v-8h8v-2h-8V3h-2v8H3z"></path>
             </svg>
+            Add Todo
         </button>
     )
 }
