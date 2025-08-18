27a5fbaab4c14dea01e4f4b6062cb1d0fec06ec2
PrateekP <prateekpanta1789@gmail.com>
2025-08-18 23:17:05 +0530

    Refactored code to use the custom storage hook

--- src/context/todoContext.jsx
+++ src/context/todoContext.jsx
@@ -1,15 +1,16 @@
 import { useState, useEffect, createContext } from "react";
+import useLocalStorage from "../hooks/useLocalStorage";
 
 export const HeaderContext = createContext();
 export const TodoContext = createContext();
 export const FormContext = createContext();
 
 export const AppProvider = ({ children }) => {
-    const [todos, setTodos] = useState([]);
+    const [todos, setTodos] = useLocalStorage();
     const totalTodos = todos.length;
     const completedTodos = todos.filter(todo => todo.isCompleted === true).length;
     const [formState, setFormState] = useState(false);
-    const isEditing = false;
+    const [isEditing, setEditing] = useState(false);
 
     const createTodo = (title, summary, priority) => {
         if (title && summary) {
@@ -39,9 +40,14 @@ export const AppProvider = ({ children }) => {
         setTodos(prev => prev.map(todo => todo.id === id ? {...todo, isCompleted: !todo.isCompleted} : todo));
     }
 
+    const editTodo = (id) => {
+        setEditing(true);
+        setFormState(true);
+    }
+
     return (
         <HeaderContext.Provider value={{ totalTodos, completedTodos }}>
-            <TodoContext.Provider value={{ todos, checkTodo }}>
+            <TodoContext.Provider value={{ todos, checkTodo, editTodo }}>
                 <FormContext.Provider value={{ isEditing, formState, setFormState, addTodo, closeTodoForm }}>
                     { children }
                 </FormContext.Provider>
