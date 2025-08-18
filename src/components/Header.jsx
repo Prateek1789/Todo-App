28822853918ba3134217b3aee7782f2ee6570d7d
PrateekP <prateekpanta1789@gmail.com>
2025-08-18 23:18:38 +0530

    Restyled progress bar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,8 +9,8 @@ const Header = () => {
         <header className='w-full h-auto text-gray-800 p-4 rounded-lg flex flex-col gap-4'>
             <h1 className='text-xl'>Daily Tasks</h1>
             <p className='text-md mb-2'>{completedTodos || 0 } / {totalTodos} Tasks completed</p>
-            <span className='progress-bar w-full h-4 rounded-sm bg-gray-200 flex p-[0.1rem]'>
-                <span className={`progress-bar-fill h-full bg-[hsl(0,0%,100%)] rounded-xs`}
+            <span className='progress-bar w-full h-4 rounded-sm bg-gray-300 flex p-[0.1rem]'>
+                <span className={`progress-bar-fill h-full bg-[#1a1a1a] rounded-xs`}
                       style={{ width: `${progressBarWidth()}%` }}></span>
             </span>
         </header>
