353550ebd32ce05348ca99a8aff614b5d9f18710
PrateekP <prateekpanta1789@gmail.com>
2025-08-20 21:18:09 +0530

    Improved contexts and event handler funcitons

--- src/context/todoContext.jsx
+++ src/context/todoContext.jsx
@@ -11,6 +11,7 @@ export const AppProvider = ({ children }) => {
     const completedTodos = todos.filter(todo => todo.isCompleted === true).length;
     const [formState, setFormState] = useState(false);
     const [isEditing, setEditing] = useState(false);
+    const [editingTodo, setEditingTodo] = useState();
 
     const createTodo = (title, summary, priority) => {
         if (title && summary) {
@@ -25,30 +26,38 @@ export const AppProvider = ({ children }) => {
         }
     }
 
-    const closeTodoForm = (titleInput, summaryInput) => {
-        titleInput.current.value = '';
-        summaryInput.current.value = '';
+    const handleFormState = () => {
         setFormState(!formState);
+        if (isEditing) setEditing(false);
     }
 
     const addTodo = (title, summary, priority) => {
         const data = createTodo(title, summary, priority);
         setTodos(prev => [data, ...prev]);
+        handleFormState();
     }
 
     const checkTodo = (id) => {
         setTodos(prev => prev.map(todo => todo.id === id ? {...todo, isCompleted: !todo.isCompleted} : todo));
     }
 
-    const editTodo = (id) => {
+    const handleEdit = (id) => {
+        const data = todos.find(todo => todo.id === id);
+        setEditingTodo(data);
         setEditing(true);
         setFormState(true);
     }
 
+    const updateTodo = (newTitle, newSummary, newPriority) => {
+        const id = editingTodo.id;
+        setTodos(prev => prev.map(todo => todo.id === id ? {...todo, title: newTitle, summary: newSummary, priority: newPriority} : todo));
+        handleFormState();
+    }
+
     return (
         <HeaderContext.Provider value={{ totalTodos, completedTodos }}>
-            <TodoContext.Provider value={{ todos, checkTodo, editTodo }}>
-                <FormContext.Provider value={{ isEditing, formState, setFormState, addTodo, closeTodoForm }}>
+            <TodoContext.Provider value={{ todos, checkTodo, handleEdit }}>
+                <FormContext.Provider value={{ formState, setFormState, isEditing, editingTodo, addTodo, handleFormState, updateTodo }}>
                     { children }
                 </FormContext.Provider>
             </TodoContext.Provider>
