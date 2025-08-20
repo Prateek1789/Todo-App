8061b237cf954498d45567ee29de59344b228a0c
PrateekP <prateekpanta1789@gmail.com>
2025-08-20 22:01:18 +0530

    Added responsiveness for bigger screens

--- src/components/TaskArea.jsx
+++ src/components/TaskArea.jsx
@@ -8,7 +8,7 @@ const TaskContainer = () => {
     return (
         <main className='w-full rounded-lg flex flex-col gap-3'>
             <h2 className='text-2xl text-gray-800'>Today's Tasks</h2>
-            <div className='task-container w-full h-full rounded-lg flex flex-col gap-3 overflow-scroll'>
+            <div className='task-container w-full h-full rounded-lg flex flex-col gap-3 overflow-scroll md:grid md:grid-cols-2 md:grid-rows-[repeat(5,_minmax(0,_6rem))] xl:grid-cols-3'>
                 { todos.map((todo, idx) => <Todo key={idx} task={todo} />) }
             </div>
         </main>
