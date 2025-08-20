8b55d8dac0b08cf64b6d99bf05d4998285702726
PrateekP <prateekpanta1789@gmail.com>
2025-08-20 21:49:53 +0530

    Added delete button

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -2,7 +2,7 @@ import { useContext, useRef } from "react";
 import { TodoContext } from "../context/todoContext";
 
 const Todo = ({ task: { id, title, date, priority, isCompleted } }) => {
-  const { checkTodo, handleEdit } = useContext(TodoContext);
+  const { checkTodo, handleEdit, deleteTodo } = useContext(TodoContext);
   const todoRef = useRef();
 
   const priorityColor = {
@@ -41,6 +41,13 @@ const Todo = ({ task: { id, title, date, priority, isCompleted } }) => {
               onClick={() => handleEdit(Number(todoRef.current.dataset.id))}>
         <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="#ffffff" viewBox="0 0 24 24" className="m-auto" >
           <path d="m19.41,3c-.78-.78-2.05-.78-2.83,0L4.29,15.29c-.13.13-.22.29-.26.46l-1,4c-.08.34.01.7.26.95.19.19.45.29.71.29.08,0,.16,0,.24-.03l4-1c.18-.04.34-.13.46-.26l12.29-12.29c.78-.78.78-2.05,0-2.83l-1.59-1.59Zm-11.93,15.1l-2.11.53.53-2.11L15,7.41l1.59,1.59-9.1,9.1Zm10.51-10.51l-1.59-1.59,1.59-1.59,1.59,1.58-1.59,1.59Z"></path>
+        </svg>
+      </button>
+      <button type="button" 
+              className="p-0.5 ml-2 bg-transparent rounded-md"
+              onClick={() => deleteTodo(Number(todoRef.current.dataset.id))}>
+        <svg xmlns="http://www.w3.org/2000/svg" height="20" width="20" fill="#ffffff" viewBox="0 -960 960 960" className="m-auto">
+          <path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"/>
         </svg>
       </button>
     </div>
