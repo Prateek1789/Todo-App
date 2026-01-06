571577a4bd0e621bf42861ec20aa1a2c6ed2618f
PrateekP <prateekpanta1789@gmail.com>
2026-01-06 15:34:31 +0530

    Simplified component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,20 +1,23 @@
-import { useContext } from "react"
-import { HeaderContext } from "../context/todoContext"
+// import { useTodo } from "../context/TodoContext";
 
-const Header = () => {
-    const { totalTodos, completedTodos } = useContext(HeaderContext);
-    const progressBarWidth = () => (100 / totalTodos) * completedTodos;
+interface HeaderProps {};
+
+const Header = ({}: HeaderProps) => {
+    /* const { totalTodos, completedTodos } = useTodo();
+    const progressBarWidth = () => (100 / totalTodos) * completedTodos; */
 
     return (
         <header className='w-full h-auto text-gray-800 p-4 rounded-lg flex flex-col gap-4'>
-            <h1 className='text-xl'>Daily Tasks</h1>
-            <p className='text-md mb-2'>{completedTodos || 0 } / {totalTodos} Tasks completed</p>
-            <span className='progress-bar w-full h-4 rounded-sm bg-gray-300 flex p-[0.1rem]'>
-                <span className={`progress-bar-fill h-full bg-[var(--color-text)] rounded-xs`}
-                      style={{ width: `${progressBarWidth()}%` }}></span>
-            </span>
+            <h1 className='text-xl'>Todo List</h1>
+            <div className="w-full h-auto">
+                <p className='text-md mb-2'>{0} / {0} Tasks completed</p>
+                <span className='progress-bar w-full h-4 rounded-sm bg-gray-200 flex p-[0.15rem]'>
+                    <span className={`progress-bar-fill h-full bg-neutral-800 rounded-xs`}
+                          style={{ width: `${100 / 4}%` }}></span>
+                </span>
+            </div>
         </header>
     )
-}
+};
 
 export default Header
