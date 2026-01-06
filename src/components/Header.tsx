dce5e6980c1e24b87b9913aa3219530c82c51154
PrateekP <prateekpanta1789@gmail.com>
2026-01-06 21:41:39 +0530

    Added the context and total nd completed todo counter values

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,19 +1,19 @@
-// import { useTodo } from "../context/TodoContext";
+import { useTodo } from "../context/TodoContext";
 
 interface HeaderProps {};
 
 const Header = ({}: HeaderProps) => {
-    /* const { totalTodos, completedTodos } = useTodo();
-    const progressBarWidth = () => (100 / totalTodos) * completedTodos; */
+    const { total, completed } = useTodo();
+    const barWidth = (100 / total) * completed;
 
     return (
         <header className='w-full h-auto text-gray-800 p-4 rounded-lg flex flex-col gap-4'>
             <h1 className='text-xl'>Todo List</h1>
             <div className="w-full h-auto">
-                <p className='text-md mb-2'>{0} / {0} Tasks completed</p>
+                <p className='text-md mb-2'>{completed} / {total} Tasks completed</p>
                 <span className='progress-bar w-full h-4 rounded-sm bg-gray-200 flex p-[0.15rem]'>
                     <span className={`progress-bar-fill h-full bg-neutral-800 rounded-xs`}
-                          style={{ width: `${100 / 4}%` }}></span>
+                          style={{ width: `${barWidth}%` }}></span>
                 </span>
             </div>
         </header>
