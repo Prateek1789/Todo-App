d2c4222635620f50ec49537b7c3968d2601648a8
PrateekP <prateekpanta1789@gmail.com>
2026-01-08 23:26:33 +0530

    Added check, edit, and delete functionality

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,39 +1,60 @@
-import { useRef } from "react";
+import { useState, useRef, type KeyboardEvent } from "react";
+import { useTodo } from "../context/TodoContext";
 import { Pencil, Trash2 } from "lucide-react";
-import { type TodoShape } from "../types/types";
+import { type TodoVals } from "../context/TodoContext";
 
-const Todo = ({ id, title, priority }: TodoShape) => {
+const Todo = ({ id, title, status }: TodoVals) => {
 
+  const { updateTodo, toggleTodo, deleteTodo } = useTodo();
+  const [isEditing, setEditing] = useState(false);
+  const [editingText, setEditingText] = useState(title);
   const todoRef = useRef<HTMLDivElement | null>(null);
-  const pColors = {
-    high: 'from-PH to-background',
-    medium: 'from-PM to-background',
-    low: 'from-PL to-background',
+
+  const updateTitle = () => {
+    updateTodo(id, editingText);
+    setEditing(false);
+  };
+
+  const handleEnter = (e: KeyboardEvent) => {
+    if (e.key === 'Enter') updateTitle();
+    if (e.key === 'Escape') setEditing(false);
   };
 
   return (
     <div ref={todoRef}
          data-id={id}
-         className={`w-full h-full pl-8 flex justify-between rounded-md bg-linear-to-r from-3% to-3% ${pColors[priority]}`}>
-      <div className="h-full w-full py-2 flex items-center">
+         className={`w-full h-full px-6 flex justify-between rounded-2xl bg-background shadow-lg shadow-neutral-300`}>
+      <div className="h-full w-full py-2 flex items-center gap-4">
+        <input type="checkbox" 
+               name="todo-check"
+               id={`check-todo-${id}`}
+               checked={status}
+               disabled={isEditing}
+               onChange={() => toggleTodo(id)}
+               className="appearance-none relative w-5 h-5 border border-neutral-300 rounded-md checked:bg-green-100 checked:border-green-400 peer transition-colors duration-200 cursor-pointer"/>
         <label htmlFor={`check-todo-${id}`}
-               className="flex gap-2 text-lg md:text-xl has-checked:line-through">
-          <input type="checkbox" 
-                 name="todo-check" 
-                 id={`check-todo-${id}`} />
+               className={`${isEditing ? 'hidden': 'block'} text-lg md:text-xl peer-checked:line-through peer-checked:text-neutral-400 peer-checked:text-sm peer-checked:select-none transition-all duration-200 cursor-pointer`}>
           {title}
         </label>
+        <input type="text"
+               autoFocus
+               value={editingText}
+               name="editing_input" 
+               id={`editing-input-${id}`} 
+               className={`${isEditing ? "block" : "hidden"} border-0 outline-0 border-b border-blue-400`}
+               onChange={(e) => setEditingText(e.target.value)}
+               onKeyDown={handleEnter} />
       </div>
-      <div className="h-full p-2 flex items-center gap-2 rounded-md">
+      <div className="h-full py-2 flex items-center gap-2 rounded-md">
         <button type="button"
-                className="p-1 rounded-md"
-                onClick={() => console.log('Edit clicked')}>
-          <Pencil size={16} color="white"/>
+                className={`todo-btn btn-edit ${isEditing ? 'hidden' : 'inline'} p-1.5 rounded-lg hover:bg-blue-100 peer`}
+                onClick={() => setEditing(prev => !prev)}>
+          <Pencil size={18} className="transition-color duration-200"/>
         </button>
         <button type="button"
-                className="p-1 rounded-md"
-                onClick={() => console.log('Delete clicked')}>
-          <Trash2 size={16} color="white" />
+                className="todo-btn btn-del p-1.5 rounded-lg hover:bg-red-100"
+                onClick={() => deleteTodo(id)}>
+          <Trash2 size={18} className="transition-color duration-200" />
         </button>
       </div>
     </div>
@@ -41,9 +62,3 @@ const Todo = ({ id, title, priority }: TodoShape) => {
 };
 
 export default Todo
-
-// checked={status ? true : false}
-// ${priorityColor[priority]}
-// onChange={() => checkTodo(Number(todoRef.current.dataset.id))}
-// onClick={() => handleEdit(Number(todoRef.current.dataset.id))}
-// onClick={() => deleteTodo(Number(todoRef.current?.dataset?.id))}
