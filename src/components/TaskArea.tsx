0c821a6d52daa211e99ff92064ad82da4fdade92
PrateekP <prateekpanta1789@gmail.com>
2026-01-08 19:30:59 +0530

    Removed dummy data, added real todo data, and fixed layout

--- src/components/TaskArea.tsx
+++ src/components/TaskArea.tsx
@@ -1,39 +1,15 @@
 import Todo from "./Todo";
-import { type TodoShape } from "../types/types";
+import { useTodo } from "../context/TodoContext";
 
 interface TaskAreaProps {};
 
 const TaskArea = ({}: TaskAreaProps) => {
-
-    const todoItems: TodoShape[] = [  // Test data
-        {
-            id: 131,
-            title: "Fix app",
-            date: "05-01-26",
-            priority: "high",
-            status: false
-        },
-        {
-            id: 132,
-            title: "Make another App",
-            date: "05-01-26",
-            priority: "medium",
-            status: false
-        },
-        {
-            id: 133,
-            title: "Practice the shit out of it",
-            date: "05-01-26",
-            priority: "low",
-            status: false
-        },
-    ];
-
-    const totalTodos = todoItems.length; // test data
+    const { todos, total } = useTodo();
 
     return (
-        <div className={`w-full h-full rounded-lg border border-neutral-200 grid grid-rows-[repeat(${totalTodos},3.5rem)] content-start gap-3`}>
-            { todoItems.map(itm => <Todo key={itm.id} 
+        <div className={`w-full h-full rounded-2xl grid content-start gap-3 overflow-scroll`}
+             style={{ gridTemplateRows: `repeat(${total},4.25rem)` }}>
+            { todos?.map(itm => <Todo key={itm.id} 
                                          id={itm.id} 
                                          title={itm.title} 
                                          date={itm.date} 
