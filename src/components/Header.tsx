0b119f395271ff83f6aa4d64ee8b3f6c1d974b71
PrateekP <prateekpanta1789@gmail.com>
2026-01-08 13:46:13 +0530

    Redefined header structure

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,16 +5,23 @@ interface HeaderProps {};
 const Header = ({}: HeaderProps) => {
     const { total, completed } = useTodo();
     const barWidth = (100 / total) * completed;
+    const date = new Date().toLocaleDateString("en-us", { dateStyle: "long" });
 
     return (
-        <header className='w-full h-auto text-gray-800 p-4 rounded-lg flex flex-col gap-4'>
-            <h1 className='text-xl'>Todo List</h1>
-            <div className="w-full h-auto">
-                <p className='text-md mb-2'>{completed} / {total} Tasks completed</p>
-                <span className='progress-bar w-full h-4 rounded-sm bg-gray-200 flex p-[0.15rem]'>
-                    <span className={`progress-bar-fill h-full bg-neutral-800 rounded-xs`}
-                          style={{ width: `${barWidth}%` }}></span>
+        <header className='w-full h-auto text-gray-800 rounded-lg flex flex-col gap-4'>
+            <h3 className="font-extrabold text-gray-400">{date}</h3>
+            <div className="flex justify-between items-end">
+                <h1 className="text-sm font-bold">My Day</h1>
+                <span className="flex gap-1 items-baseline text-blue-400 select-none">
+                    <p className="text-lg font-bold">{completed}</p>
+                    / 
+                    <p className="text-sm">{total}</p>
                 </span>
+            </div>
+            <div className="w-full h-auto flex items-center">
+                <progress max='100'
+                          value={barWidth} 
+                          className="w-full h-2 rounded-lg" />
             </div>
         </header>
     )
