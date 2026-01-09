5815ae45000e8ca92091801740615be61ec7a5cc
PrateekP <prateekpanta1789@gmail.com>
2026-01-09 12:37:19 +0530

    Added visual state for empty todo list

--- src/components/TaskArea.tsx
+++ src/components/TaskArea.tsx
@@ -1,5 +1,6 @@
 import Todo from "./Todo";
 import { useTodo } from "../context/TodoContext";
+import { CircleCheck } from "lucide-react";
 
 interface TaskAreaProps {};
 
@@ -7,13 +8,20 @@ const TaskArea = ({}: TaskAreaProps) => {
     const { todos, total } = useTodo();
 
     return (
-        <div className={`w-full h-full rounded-2xl grid content-start gap-3 overflow-scroll`}
+        <div className={`w-full h-full px-4 relative rounded-2xl grid content-start gap-3 overflow-y-scroll`}
              style={{ gridTemplateRows: `repeat(${total},4.25rem)` }}>
             { todos?.map(itm => <Todo key={itm.id} 
                                          id={itm.id} 
                                          title={itm.title} 
                                          date={itm.date} 
                                          status={itm.status} /> )}
+            <div className={`w-full h-50 border border-blue-400 border-dashed rounded-2xl ${!total ? 'flex' : 'hidden'} flex-col justify-center gap-4 items-center`}>
+                <CircleCheck size={50} color="oklch(70.7% 0.165 254.624)" />
+                <span className="text-center">
+                    <h4 className="text-lg font-semibold text-neutral-500">All cought up!</h4>
+                    <p className="text-sm text-neutral-400">Take a break or add a new task above.</p>
+                </span>
+            </div>
         </div>
     )
 };
