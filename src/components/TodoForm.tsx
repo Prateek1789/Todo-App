e12794c14425c146dc6a5321e43390013e50ebd5
PrateekP <prateekpanta1789@gmail.com>
2026-01-11 14:47:00 +0530

    Added a button to select priority for a task

--- src/components/TodoForm.tsx
+++ src/components/TodoForm.tsx
@@ -1,39 +1,49 @@
-import { useState, useRef, type FormEvent } from 'react';
+import { useState, type FormEvent } from 'react';
 import { useTodo } from '../context/TodoContext';
-import { Plus } from 'lucide-react';
+import { Plus, Flag } from 'lucide-react';
 
 interface TodoFormProps {};
 
 const TodoForm = ({}: TodoFormProps) => {
     const [task, setTask] = useState('');
-    const { addTodo } = useTodo();
-    const inputRef = useRef<HTMLInputElement | null>(null);
+    const { priority, cyclePriority, addTodo } = useTodo();
+
+    const priorityColors = {
+        low: 'oklch(62.3% 0.214 259.815)',
+        medium: 'oklch(76.9% 0.188 70.08)',
+        high: ' oklch(63.7% 0.237 25.331)'
+    };
 
     const submit = (e: FormEvent) => {
         e.preventDefault();
-       addTodo(task);
+       addTodo(task, priority);
        setTask('');
     };
 
     return (
         <div className='w-full h-auto px-4'>
-        <form action=""
-              onSubmit={submit}
-              className="w-full h-22 flex items-center gap-4 px-4 py-2 rounded-2xl bg-background has-focus:outline-2 outline-blue-500 shadow-lg has-focus:shadow-blue-100">
-            <button type="submit"
-                    className='w-10 h-10 grid place-content-center bg-blue-400 disabled:bg-blue-100 rounded-xl shrink-0'
-                    onClick={() => {}}
-                    disabled={task ? false : true}>
-                <Plus color="white" />
-            </button>
-            <input ref={inputRef}
-                   type="text" 
-                   name="task_input" 
-                   id="task-input"
-                   className='w-full h-full rounded-xl focus:outline-0'
-                   placeholder="Add new task..."
-                   value={task} 
-                   onChange={(e) => setTask(e.target.value)}/>
+            <form action=""
+                  onSubmit={submit}
+                  className="w-full h-18 flex items-center gap-4 px-4 py-2 rounded-3xl bg-background has-focus:outline-2 outline-blue-500 shadow-lg has-focus:shadow-blue-100">
+                <button type="submit"
+                        className='w-10 h-10 grid place-content-center bg-blue-400 disabled:bg-blue-100 rounded-xl shrink-0'
+                        disabled={task ? false : true}>
+                    <Plus color="white" />
+                </button>
+                <input  type="text" 
+                        name="task_input" 
+                        id="task-input"
+                        className='w-full h-full rounded-xl focus:outline-0'
+                        placeholder="Add new task..."
+                        value={task} 
+                        onChange={(e) => setTask(e.target.value)}/>
+                <button type="button"
+                        className='w-10 h-10 grid place-content-center bg-neutral-100 rounded-xl shrink-0'
+                        onClick={cyclePriority}
+                        title={`Priority: ${priority}`}>
+                    <Flag color={priorityColors[priority]}
+                          strokeWidth='3px'/>
+                </button>
         </form>
         </div>
     )
