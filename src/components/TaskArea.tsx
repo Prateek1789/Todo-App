7ecea9b6e0ffd211bfa65684bbbd84eb094c5fdb
PrateekP <prateekpanta1789@gmail.com>
2026-01-11 14:48:07 +0530

    Used sorted todos to render the list of todos and added priority prop for the Todo component

--- src/components/TaskArea.tsx
+++ src/components/TaskArea.tsx
@@ -5,16 +5,17 @@ import { CircleCheck } from "lucide-react";
 interface TaskAreaProps {};
 
 const TaskArea = ({}: TaskAreaProps) => {
-    const { todos, total } = useTodo();
+    const { sortedTodos, total } = useTodo();
 
     return (
         <div className={`w-full h-full px-4 relative rounded-2xl grid content-start gap-3 overflow-y-scroll`}
              style={{ gridTemplateRows: `repeat(${total},4.25rem)` }}>
-            { todos?.map(itm => <Todo key={itm.id} 
-                                         id={itm.id} 
-                                         title={itm.title} 
-                                         date={itm.date} 
-                                         status={itm.status} /> )}
+            { sortedTodos?.map(itm => <Todo key={itm.id} 
+                                      id={itm.id} 
+                                      title={itm.title} 
+                                      date={itm.date} 
+                                      status={itm.status}
+                                      priority={itm.priority} /> )}
             <div className={`w-full h-50 border border-blue-400 border-dashed rounded-2xl ${!total ? 'flex' : 'hidden'} flex-col justify-center gap-4 items-center`}>
                 <CircleCheck size={50} color="oklch(70.7% 0.165 254.624)" />
                 <span className="text-center">
